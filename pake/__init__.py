e29d96073a48164fbb1d68edc1f356ad84ab5b1e
kshramt <kshramt@example.com>
2017-02-13 02:34:21 +0900

    Multithreading

--- pake/__init__.py
+++ pake/__init__.py
@@ -1,7 +1,10 @@
 import argparse
+import concurrent.futures
 import os
+import queue
 import subprocess
 import sys
+import threading
 import warnings
 from typing import (
     Any,
@@ -15,6 +18,11 @@ from typing import (
 __version__ = "0.1.0"
 
 
+class Err(Exception):
+    def __init__(self, msg=""):
+        self.msg=msg
+
+
 class _Nil:
     __slots__ = ()
 
@@ -23,11 +31,6 @@ class _Nil:
 
 
 _nil = _Nil()
-
-
-class Err(Exception):
-    def __init__(self, msg=""):
-        self.msg=msg
 
 
 class _Cons:
@@ -47,14 +50,30 @@ class _Job:
         self.ts = ts
         self.ds = ds
         self.unique_ds = _unique(ds)
-        self.n_rest = len(self.unique_ds)
+        self._n_rest = len(self.unique_ds)
         self.visited = False
+        self._lock = threading.Lock()
+
+    def __str__(self):
+        return f"{type(self).__name__}({self.ts}, {self.ds})"
 
     def rm_targets(self):
         pass
 
     def need_update(self):
         return True
+
+    def n_rest(self):
+        with self._lock:
+            return self._n_rest
+
+    def dec_n_rest(self):
+        with self._lock:
+            self._n_rest -= 1
+
+    def set_n_rest(self, x):
+        with self._lock:
+            self.n_rest = x
 
 
 class _PhonyJob(_Job):
@@ -86,7 +105,6 @@ class DSL:
         self._job_of_target: Dict[str, _Job] = dict()
         self._f_of_phony: Dict[str, Callable[[_Job], Any]] = dict()
         self._deps_of_phony: Dict[str, List[str]] = dict()
-        self._phonies: Set[str] = set()
 
     def task(self, targets: List[str], deps: List[str]) -> Callable[[_Job], Any]:
         def _(f: Callable[[_Job], Any]) -> Callable[[_Job], Any]:
@@ -98,7 +116,7 @@ class DSL:
 
     def phony(self, target: str, deps: List[str]) -> Callable[[Callable[[_Job], Any]], Callable[[_Job], Any]]:
         self._deps_of_phony.setdefault(target, []).extend(deps)
-        self._phonies.add(target)
+
         def _(f: Callable[[_Job], Any]) -> Callable[[_Job], Any]:
             _set_unique(self._f_of_phony, target, f)
             return _do_nothing
@@ -121,7 +139,7 @@ class DSL:
                 target,
                 self._job_of_target,
                 self.task,
-                self._phonies,
+                self._deps_of_phony,
                 _nil,
             )
         _process_jobs(leaf_jobs, dependent_jobs, keep_going, n_jobs)
@@ -135,55 +153,7 @@ class DSL:
         )
 
 
-class _TaskPool:
-    def __init__(self, dependent_jobs, keep_going, n_jobs_max):
-        assert n_jobs_max > 0
-        self._stack = []
-        self._tasks = set()
-        self._all_tasks = []
-        self._defered_errors = []
-        self.dependent_jobs = dependent_jobs
-        self.keep_going = keep_going
-        self.n_jobs_max = n_jobs_max
-
-    def wait_all_tasks(self):
-        i = -1
-        while True:
-            i += 1
-            if len(self._all_tasks) < i:
-                return
-
-    def push_job(self, j):
-        self._stack.append(j)
-        while self._stack:
-            j = self._stack.pop()
-            # Start executing the job
-            # `Job` is called only once
-            assert j.n_rest == 0
-            got_error = False
-            if j.need_update():
-                try:
-                    j.f(j)
-                except Exception as e:
-                    got_error = True
-                    # Use string interpolation for async output
-                    warnings.warn(f"{repr(e)}\t{j}")
-                    j.rm_targets()
-                    if self.keep_going:
-                        self._defered_errors.append((j, e))
-                    else:
-                        raise e
-                    j.n_rest = -1
-            if not got_error:
-                for t in j.ts:
-                    # top targets does not have dependent jobs
-                    for dj in self.dependent_jobs.setdefault(t, []):
-                        dj.n_rest -= 1
-                        if dj.n_rest == 0:
-                            self.push_job(dj)
-
-
-def sh(s, stdout=None):
+def sh(s: str, stdout=None):
     print(s, file=sys.stderr)
     return subprocess.run(
         s,
@@ -196,7 +166,7 @@ def sh(s, stdout=None):
     )
 
 
-def rm(path):
+def rm(path: str) -> Any:
     print(f"os.remove({repr(path)})", file=sys.stderr)
     try:
         os.remove(path)
@@ -204,7 +174,11 @@ def rm(path):
         pass
 
 
-def _collect_phonies(job_of_target, deps_of_phony, f_of_phony):
+def _collect_phonies(
+        job_of_target: Dict[str, _Job],
+        deps_of_phony: Dict[str, List[str]],
+        f_of_phony: Dict[str, Callable[[_Job], Any]],
+) -> Any:
     for target, deps in deps_of_phony.items():
         _set_unique(
             job_of_target,
@@ -218,11 +192,11 @@ def _collect_phonies(job_of_target, deps_of_phony, f_of_phony):
 
 
 def _make_graph(
-        dependent_jobs,
-        leaf_jobs,
-        target,
-        job_of_target,
-        make_job,
+        dependent_jobs: Dict[str, List[_Job]],
+        leaf_jobs: List[_Job],
+        target: str,
+        job_of_target: Dict[str, _Job],
+        task,
         phonies,
         call_chain,
 ):
@@ -231,7 +205,7 @@ def _make_graph(
     if target not in job_of_target:
         assert target not in phonies
         if os.path.lexists(target):
-            @make_job([target], [])
+            @task([target], [])
             def _(j):
                 raise Err(f"Must not happen: job for leaf node {repr(target)} called")
         else:
@@ -248,7 +222,7 @@ def _make_graph(
             leaf_jobs,
             dep,
             job_of_target,
-            make_job,
+            task,
             phonies,
             current_call_chain,
         )
@@ -256,16 +230,110 @@ def _make_graph(
 
 
 def _process_jobs(jobs, dependent_jobs, keep_going, n_jobs):
-    task_pool = _TaskPool(dependent_jobs, keep_going, n_jobs)
+    tp = _ThreadPool(dependent_jobs, keep_going, n_jobs)
+    defered_errors = queue.Queue()
     for j in jobs:
-        task_pool.push_job(j)
-    task_pool.wait_all_tasks()
-    if task_pool._defered_errors:
+        tp.push_job(j)
+    tp.wait()
+    if defered_errors.qsize() > 0:
         warnings.warn("Following errors have thrown during the execution")
-        for j, e in task_pool._defered_errors:
+        for _ in range(defered_errors.qsize()):
+            j, e = defered_errors.get()
             warnings.warn(repr(e))
             warnings.warn(j)
         raise Err("Execution failed.")
+
+
+class _TSet:
+    def __init__(self):
+        self._lock = threading.Lock()
+        self._set = set()
+
+    def __len__(self):
+        with self._lock:
+            return len(self._set)
+
+    def add(self, x):
+        with self._lock:
+            self._set.add(x)
+
+    def remove(self, x):
+        with self._lock:
+            self._set.remove(x)
+
+    def pop(self):
+        with self._lock:
+            return self._set.pop()
+
+
+class _ThreadPool:
+    def __init__(self, dependent_jobs, keep_going, n_max):
+        assert n_max > 0
+        self._dependent_jobs = dependent_jobs
+        self._keep_going = keep_going
+        self._n_max = n_max
+        self._threads = _TSet()
+        self._threads_loc = threading.Lock()
+        self._queue = queue.Queue()
+
+    def push_job(self, j):
+        self._queue.put(j)
+        with self._threads_loc:
+            if len(self._threads) < self._n_max:
+                t = threading.Thread(
+                    target=self._worker,
+                    daemon=True,
+                )
+                self._threads.add(t)
+                t.start()
+
+    def wait(self):
+        while True:
+            try:
+                t = self._threads.pop()
+            except KeyError as e:
+                break
+            t.join()
+
+    def _worker(self):
+        try:
+            while True:
+                j = self._deq()
+                if not j:
+                    break
+                assert j.n_rest() == 0
+                got_error = False
+                if j.need_update():
+                    try:
+                        j.f(j)
+                    except Exception as e:
+                        got_error = True
+                        warnings.warn(f"{repr(e)}\t{j}")
+                        j.rm_targets()
+                        if self._keep_going:
+                            defered_errors.put((j, e))
+                        else:
+                            raise e
+                    j.set_n_rest(-1)
+                if not got_error:
+                    for t in j.ts:
+                        # top targets does not have dependent jobs
+                        for dj in self._dependent_jobs.get(t, ()):
+                            dj.dec_n_rest()
+                            if dj.n_rest() == 0:
+                                self.push_job(dj)
+        finally:
+            with self._threads_loc:
+                try:
+                    self._threads.remove(threading.current_thread())
+                except:
+                    pass
+
+    def _deq(self):
+        try:
+            return self._queue.get(block=True, timeout=0.02)
+        except queue.Empty:
+            return False
 
 
 def _parse_argv(argv):
@@ -280,13 +348,13 @@ def _parse_argv(argv):
         version=f"%(prog)s {__version__}",
     )
     parser.add_argument(
-        "--jobs",
+        "-j", "--jobs",
         type=int,
         default=1,
         help="Number of parallel external jobs.",
     )
     parser.add_argument(
-        "--keep-going",
+        "-k", "--keep-going",
         action="store_true",
         default=False,
         help="Keep going unrelated jobs even if some jobs fail.",
--- pakefile.py
+++ pakefile.py
@@ -20,20 +20,36 @@ rm = pake.rm
 
 
 all_files = sh("git ls-files -z", stdout=subprocess.PIPE).stdout.split("\0")
-all_tests_sh_files = [path for path in all_files if path.startswith(f"tests{os.path.sep}") and path.endswith(".sh")]
-all_pake_py_files = [path for path in all_files if path.startswith(f"pake{os.path.sep}") and path.endswith(".py")]
+tests_sh_files = [path for path in all_files if path.startswith(f"tests{os.path.sep}") and path.endswith(".sh")]
+tests_py_files = [path for path in all_files if path.startswith(f"tests{os.path.sep}") and path.endswith(".py")]
+pake_py_files = [path for path in all_files if path.startswith(f"pake{os.path.sep}") and path.endswith(".py")]
 
 
 def let():
-    def let():
-        for test_sh in all_tests_sh_files:
-            test_sh_done = test_sh + ".done"
-            phony("default", [test_sh_done])
+    phony("default", ["check"])
 
-            @task([test_sh_done], [test_sh] + all_pake_py_files)
+    def let():
+        for test_sh in tests_sh_files:
+            test_sh_done = test_sh + ".done"
+            phony("check", [test_sh_done])
+
+            @task([test_sh_done], [test_sh] + pake_py_files)
             def _(j):
                 sh(f"""
                 {j.ds[0]}
+                touch {j.ts[0]}
+                """)
+    let()
+
+    def let():
+        for test_py in tests_py_files:
+            test_py_done = test_py + ".done"
+            phony("check", [test_py_done])
+
+            @task([test_py_done], [test_py] + pake_py_files)
+            def _(j):
+                sh(f"""
+                {os.environ["PYTHON"]} {j.ds[0]}
                 touch {j.ts[0]}
                 """)
     let()
