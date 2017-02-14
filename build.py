ae4b85aae4270531b94218f0a061478461ab26a2
kshramt <kshramt@example.com>
2017-02-15 06:08:56 +0900

    Fix

--- build.py
+++ build.py
@@ -52,35 +52,36 @@ def let():
 
     phony("check", [], desc="Run tests")
     for v in vs:
-        v_files = [path for path in all_files if path.startswith(os.path.join("buildpy", v))]
-        v_test_files = [path for path in v_files if path.startswith(os.path.join("buildpy", v, "tests"))]
-        def let():
-            for test_sh in [path for path in v_test_files if path.endswith(".sh")]:
-                test_sh_done = test_sh + ".done"
-                phony("check", [test_sh_done])
+        def let(v=v):
+            v_files = [path for path in all_files if path.startswith(os.path.join("buildpy", v))]
+            v_test_files = [path for path in v_files if path.startswith(os.path.join("buildpy", v, "tests"))]
+            def let():
+                for test_sh in [path for path in v_test_files if path.endswith(".sh")]:
+                    test_sh_done = test_sh + ".done"
+                    phony("check", [test_sh_done])
 
-                @file([test_sh_done], [test_sh] + buildpy_py_files, desc="Test {}".format(test_sh))
-                def _(j):
-                    sh("""
-                    {}
-                    touch {}
-                    """.format(j.ds[0], j.ts[0]))
-        let()
+                    @file([test_sh_done], [test_sh] + buildpy_py_files, desc="Test {}".format(test_sh))
+                    def _(j):
+                        sh("""
+                        {}
+                        touch {}
+                        """.format(j.ds[0], j.ts[0]))
+            let()
 
-        def let():
-            for test_py in [path for path in v_test_files if path.endswith(".py")]:
-                test_py_done = test_py + ".done"
-                phony("check", [test_py_done])
+            def let():
+                for test_py in [path for path in v_test_files if path.endswith(".py")]:
+                    test_py_done = test_py + ".done"
+                    phony("check", [test_py_done])
 
-                @file([test_py_done], [test_py] + buildpy_py_files, desc="Test {}".format(test_py))
-                def _(j):
-                    sh("""
-                    {} {}
-                    touch {}
-                    """.format(os.environ["PYTHON"], j.ds[0], j.ts[0]))
+                    @file([test_py_done], [test_py] + buildpy_py_files, desc="Test {}".format(test_py))
+                    def _(j):
+                        sh("""
+                        {} {}
+                        touch {}
+                        """.format(os.environ["PYTHON"], j.ds[0], j.ts[0]))
+            let()
         let()
 let()
-
 
 if __name__ == '__main__':
     __dsl.main(sys.argv)
