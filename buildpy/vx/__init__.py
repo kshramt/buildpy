a771adc9a4ea8c8c7b4d2eead9d33e214e513e45
kshramt <kshramt@example.com>
2017-08-12 13:33:32 +0900

    HFS has only 1 s time resolution and thus d.mtime == t.mtime is possible

--- buildpy/vx/__init__.py
+++ buildpy/vx/__init__.py
@@ -165,7 +165,7 @@ class _FileJob(_Job):
         if not self.unique_ds:
             return False
         ts_mtime_min = min(t.st_mtime for t in stat_ts)
-        return any(os.path.getmtime(d) >= ts_mtime_min for d in self.unique_ds)
+        return any(os.path.getmtime(d) > ts_mtime_min for d in self.unique_ds):
 
 
 class _ThreadPool:
