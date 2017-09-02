a8bc19373f8b58de07848d6972d11d48c2127b42
kshramt <kshramt@example.com>
2017-09-03 00:31:41 +0900

    Flat is better than nested

--- build.py
+++ build.py
@@ -39,53 +39,49 @@ test_files = set(path for path in buildpy_files if re.match(os.path.join("^build
 buildpy_py_files = list(py_files.intersection(buildpy_files) - test_files)
 
 
-@let
-def _():
-    phony("all", [], desc="The default target")
+phony("all", [], desc="The default target")
 
-    @phony("sdist", [], desc="Make a distribution file")
-    def _(j):
-        sh(f"""
-        git ls-files -z |
-        xargs -0 -n1 echo include >| MANIFEST.in
-        {python} setup.py sdist
-        """)
 
-    phony("check", [], desc="Run tests")
-    for v in vs:
-        @let
-        def _(v=v):
-            v_files = [path for path in all_files if path.startswith(os.path.join("buildpy", v))]
-            v_test_files = [path for path in v_files if path.startswith(os.path.join("buildpy", v, "tests"))]
+@phony("sdist", [], desc="Make a distribution file")
+def _(j):
+    sh(f"""
+    git ls-files -z |
+    xargs -0 -n1 echo include >| MANIFEST.in
+    {python} setup.py sdist
+    """)
+
+
+phony("check", [], desc="Run tests")
+for v in vs:
+    @let
+    def _(v=v):
+        v_files = [path for path in all_files if path.startswith(os.path.join("buildpy", v))]
+        v_test_files = [path for path in v_files if path.startswith(os.path.join("buildpy", v, "tests"))]
+        for test_sh in [path for path in v_test_files if path.endswith(".sh")]:
             @let
-            def _():
-                for test_sh in [path for path in v_test_files if path.endswith(".sh")]:
-                    @let
-                    def _(test_sh=test_sh):
-                        test_sh_done = test_sh + ".done"
-                        phony("check", [test_sh_done])
+            def _(test_sh=test_sh):
+                test_sh_done = test_sh + ".done"
+                phony("check", [test_sh_done])
 
-                        @file([test_sh_done], [test_sh] + buildpy_py_files, desc=f"Test {test_sh}")
-                        def _(j):
-                            sh(f"""
-                            {j.ds[0]}
-                            touch {j.ts[0]}
-                            """)
+                @file([test_sh_done], [test_sh] + buildpy_py_files, desc=f"Test {test_sh}")
+                def _(j):
+                    sh(f"""
+                    {j.ds[0]}
+                    touch {j.ts[0]}
+                    """)
 
+        for test_py in [path for path in v_test_files if path.endswith(".py")]:
             @let
-            def _():
-                for test_py in [path for path in v_test_files if path.endswith(".py")]:
-                    @let
-                    def _(test_py=test_py):
-                        test_py_done = test_py + ".done"
-                        phony("check", [test_py_done])
+            def _(test_py=test_py):
+                test_py_done = test_py + ".done"
+                phony("check", [test_py_done])
 
-                        @file([test_py_done], [test_py] + buildpy_py_files, desc=f"Test {test_py}")
-                        def _(j):
-                            sh(f"""
-                            {python} {j.ds[0]}
-                            touch {j.ts[0]}
-                            """)
+                @file([test_py_done], [test_py] + buildpy_py_files, desc=f"Test {test_py}")
+                def _(j):
+                    sh(f"""
+                    {python} {j.ds[0]}
+                    touch {j.ts[0]}
+                    """)
 
 
 if __name__ == '__main__':
