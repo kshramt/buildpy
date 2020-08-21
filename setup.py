ddedde703ccbcc3a27b2d0b9c2c033847d3e98e7
kshramt <kshramt@users.noreply.github.com>
2020-08-21 20:14:52 +0900

    tweak:

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def getversion():
     with open(os.path.join("buildpy", "vx", "__init__.py")) as fp:
         for l in fp:
             if l.startswith(head) and l.endswith(tail):
-                return l[len(head):-len(tail)]
+                return l[len(head) : -len(tail)]
     raise Exception("__version__ not found")
 
 
@@ -24,35 +24,30 @@ setup(
         "buildpy.v2",
         "buildpy.v3",
         "buildpy.v4",
-
         "buildpy.v5",
         "buildpy.v5._convenience",
         "buildpy.v5._log",
         "buildpy.v5._tval",
         "buildpy.v5.exception",
         "buildpy.v5.resource",
-
         "buildpy.v6",
         "buildpy.v6._convenience",
         "buildpy.v6._log",
         "buildpy.v6._tval",
         "buildpy.v6.exception",
         "buildpy.v6.resource",
-
         "buildpy.v7",
         "buildpy.v7._convenience",
         "buildpy.v7._log",
         "buildpy.v7._tval",
         "buildpy.v7.exception",
         "buildpy.v7.resource",
-
         "buildpy.v8",
         "buildpy.v8._convenience",
         "buildpy.v8._log",
         "buildpy.v8._tval",
         "buildpy.v8.exception",
         "buildpy.v8.resource",
-
         "buildpy.vx",
         "buildpy.vx._convenience",
         "buildpy.vx._log",
@@ -61,14 +56,15 @@ setup(
         "buildpy.vx.resource",
     ],
     install_requires=[
-        "boto3",
-        "google-cloud-bigquery",
-        "google-cloud-storage",
-        "psutil",
+        "boto3 <2",
+        "google-cloud-bigquery <2",
+        "google-cloud-storage <2",
+        "psutil <6",
     ],
-    classifiers=[
-        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)'
-    ],
+    extras_require=dict(
+        dev=["mypy", "pyflakes", "black", "pylint", "wheel", "twine", "pytype"]
+    ),
+    classifiers=["License :: OSI Approved :: GNU General Public License v3 (GPLv3)"],
     data_files=[(".", ["LICENSE.txt"])],
     zip_safe=True,
 )
